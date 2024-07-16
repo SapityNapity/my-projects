db21a448276fc63084556ea14e8c040ce98a8e56
SapityNapity <68217871+SapityNapity@users.noreply.github.com>
2024-07-17 00:22:09 +0500

    number guessing game.c
    Edit 1: Removed the hint dialogue (redundant).
    The code now tells if the number by user is higher or lower than than the random secret number, the secret number is now random (limited to 20)

--- number guessing game.c
+++ number guessing game.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <time.h>
 int main(){
-    int secretnumber = 16;
+    srand(time(0));
+    int secretnumber = rand() % 20 +1;
+    printf("%d", secretnumber);
     int guess;
     int guesslimit = 3;
     int guesscount = 0;
@@ -11,23 +14,12 @@ int main(){
             printf("\nguess the number you have 3 guesses:");
             scanf("%d", &guess);
             guesscount++;
-            if (guess == secretnumber){
-                break;}
-
-
-            do{
-                    printf("\nneed a hint? (1 for yes, 0 for no): ");
-            scanf("%d", &hint);
-                if(hint == 1){
-                    printf("\nits between 12 to 18.\n");
+                if(guess < secretnumber){
+                printf("the secret number is higher than the one you provided.");
                 }
-                    else if(hint == 0){
-                        printf("\nokay, continue.\n");
-                    }
-                        else{
-                            printf("\nwrong value, please provide 1 or 0\n");
-                        }
-            }while(hint != 1 && hint != 0);
+                else if(guess > secretnumber){
+                    printf("the secret number is lower than the one you provided");
+                }
                 }
                 else{}
             }
